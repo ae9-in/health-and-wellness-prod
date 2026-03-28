5c36b5f24d16f8d1d700829bad15f97ae2669463
Wellspring Author <author@wellspring.com>
2026-03-28 16:50:15 +0530

    Optimize socket.io connection for Render (polling-first)

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,15 +3,26 @@ import { API_BASE } from './api';
 
 const SOCKET_URL = API_BASE.replace('/api', '');
 
+console.log('Initializing socket connection to:', SOCKET_URL);
+
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
   autoConnect: true,
+  transports: ['polling', 'websocket'], // Start with polling, then upgrade
+  reconnectionAttempts: 5,
+  timeout: 20000,
 });
 
 socket.on('connect', () => {
-  console.log('Connected to socket server');
+  console.log('Connected to socket server with ID:', socket.id);
+  console.log('Current transport:', socket.io.engine.transport.name);
 });
 
-socket.on('disconnect', () => {
-  console.log('Disconnected from socket server');
+socket.on('connect_error', (error) => {
+  console.error('Socket connection error:', error.message);
+  // If websocket fails, it will still try to keep polling if defined
+});
+
+socket.on('disconnect', (reason) => {
+  console.log('Disconnected from socket server. Reason:', reason);
 });
